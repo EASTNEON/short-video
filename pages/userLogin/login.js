a760aa6af6e8f048324cab2d0ce6b1f5941c716b
EASTNEON <cjl7042@qq.com>
2019-04-16 11:14:56 +0800

    登录
    用户名密码不能为空
    登录成功
    用户名不存在

--- pages/userLogin/login.js
+++ pages/userLogin/login.js
@@ -1,4 +1,5 @@
 // pages/userLogin/login.js
+const app = getApp()
 Page({
 
   /**
@@ -7,60 +8,57 @@ Page({
   data: {
 
   },
+  doLogin:function(e){
+    var me = this;
+    var formObject = e.detail.value;
+    var username = formObject.username;
+    var password = formObject.password;
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    //简单验证
+    if (username.length == 0 || password.length == 0) {
+      wx.showToast({
+        title: '用户名密码不能为空',
+        icon: 'none',
+        duration: 3000
+      })
+    }else{
+        var serverUrl = app.serverUrl;
+        wx.showLoading({
+          title: '请等待...',
+        })
+        //调用后端
+        wx.request({
+          url: serverUrl + '/login',
+          method: 'POST',
+          data:{
+            username:username,
+            password:password
+          },
+          header: {
+            'content-type': 'application/json' // 默认值
+          },
+          success:function(res){
+            console.log(res.data);
+            wx.hideLoading();
+            if(res.data.status == 200){
+              //登录成功跳转
+              wx.showToast({
+                title: '登录成功',
+                icon: 'success',
+                duration: 2000
+              });
+              app.userInfo = res.data.data;
+              //页面跳转
+            } else if (res.data.status == 500) {
+              //失败弹出框
+              wx.showToast({
+                title: res.data.msg,
+                icon: 'none',
+                duration: 3000
+              })
+            }
+          }
+        })
+    }
   }
 })
