584cf930f79336475ac63a22ac2cd0368e0a7458
EASTNEON <cjl7042@qq.com>
2019-04-17 11:28:38 +0800

    上传头像更新到数据库 静态资源配置 个人信息展示
    上传头像更新到数据库
    静态资源配置
    个人信息展示

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -9,7 +9,43 @@ Page({
     faceUrl: "../resource/images/noneface.png"
   },
 
+  onLoad:function(){
+    var me = this;
+    var user = app.userInfo;
+    var serverUrl = app.serverUrl;
+    wx.showLoading({
+      title: '请等待...',
+    })
+        //调用后端
+    wx.request({
+      url: serverUrl + '/user//query?userId=' + user.id,
+      method: 'POST',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data);
+        wx.hideLoading();
+        if (res.data.status == 200) {
+          var userInfo = res.data.data;
+          var faceUrl = "../resource/images/noneface.png";
+          if (userInfo.faceImage != null && userInfo.faceImage != '' && userInfo.faceImage != undefined) {
+            faceUrl = serverUrl + userInfo.faceImage;
+            }
+          me.setData({
+            faceUrl:faceUrl,
+            fansCounts:userInfo.fansCount,
+            followCounts:userInfo.followCounts,
+            receiveLikeCounts:userInfo.receiveLikeCounts,
+            nickname:userInfo.nickname
+          });
+        } 
+      }
+    })
+  },
+
   changeFace:function(){
+    var me = this;
     wx.chooseImage({
       count: 1,
       sizeType: ['compressed'],
@@ -30,15 +66,24 @@ Page({
             'content-type': 'application/json',
           },
           success: function (res) {
-            var data = res.data;
+            var data = JSON.parse(res.data);
             console.log(data);
             wx.hideLoading();
             if (data.status == 200) {
               wx.showToast({
-                title: '上传成功!~~',
+                title: '上传成功!',
                 icon: "success",
-              })
-          }
+              });
+              var imageUrl = data.data;
+              console.log(imageUrl);
+              me.setData({
+                faceUrl: serverUrl + imageUrl
+              });
+            } else if (data.status == 500){
+              wx.showToast({
+                title: data.msg,
+              });
+            }
           }
         })
       }
